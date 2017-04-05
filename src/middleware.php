b48e0465f02b0fec031aa04699eb86b5e954c303
cfalcione <caleb.falcione@gmail.com>
2017-04-04 20:26:09 -0500

    Modified insecure routes

--- src/middleware.php
+++ src/middleware.php
@@ -4,6 +4,6 @@
 $app->add(new \Slim\Middleware\JwtAuthentication([
 	"path" => ["/"],
 	"secure" => false,   // For development only
-    "passthrough" => ["/users/*", "/hello", "/papers", "/students/*", "/"],
+    "passthrough" => ["/users/login", "/users/register", "/hello"],
     "secret" => $app->getContainer()->get("settings")['JWT_secret'],
 ]));
