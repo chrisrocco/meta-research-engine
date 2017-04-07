59fb2090865290144fa2b9b3103c52b00b309c05
Christopher Rocco <chris.rocco7@gmail.com>
2017-04-07 13:40:12 -0500

    Fix

--- src/routes/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require("../src/routes/user_routes.php");
-require("../src/routes/assignment_routes.php");
-require("../src/routes/class_routes.php");
-require("../src/routes/study_routes.php");
