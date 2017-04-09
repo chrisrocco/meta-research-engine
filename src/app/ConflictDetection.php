db5fd102f15531fda3a4efc0f268e510e1c912a9
cfalcione <caleb.falcione@gmail.com>
2017-04-08 21:32:49 -0500

    Created structure for conflict detection

---
+++ src/app/ConflictDetection.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Caleb Falcione
+ * Date: 4/8/2017
+ * Time: 7:47 PM
+ */
+class ConflictDetection
+{
+    function newConflict ($encodingA, $encodingB) {
+        $conflict = Conflict($encodingA, $encodingB);
+
+        //Structure
+        if (count($encodingA->branches) != count($encodingB->branches)) {
+            $structureError = [
+                'branchesA' => "",
+                'branchesB' => ""
+            ];
+            $conflict->addError("structure", $structureError);
+        }
+
+        //Scope
+         
+
+        //Value
+
+
+        return $conflict;
+    }
+}
+
+class Conflict {
+
+    private $encodingA;
+    private $encodingB;
+    private $errors;
+
+    function __construct($encodingA, $encodingB) {
+        $this->encodingA = $encodingA;
+        $this->encodingB = $encodingB;
+        $this->errors = array();
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    public function addError($type, $body) {
+        if ($type !== "structure" ||
+            $type !== "scope" ||
+            $type !== "value") {
+            //TODO: Throw an error
+        }
+        if (!isset($body)) {
+            //TODO: Throw a different error
+        }
+
+        $this->errors[] = [
+            'type' => $type,
+            'body' => $body
+        ];
+    }
+
+
+}
