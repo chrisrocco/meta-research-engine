92e9fb02ec2d949dc9eb99b4d3f1d071a630e6c5
Chris Rocco <chris.rocco7@gmail.com>
2017-04-10 18:28:58 -0500

    Conflict Management

--- src/app/ConflictManager.php
+++ src/app/ConflictManager.php
@@ -2,34 +2,34 @@
 class ConflictManager {
 
     function compare($assignments_array){
-        $conflicts = [];
-
-        $conflicts[] = [
-            "Structure Conflicts" => $this->compareStructures($assignments_array)
-        ];
-        $conflicts[] = [
-            "Scope Conflicts" => $this->compareScopes($assignments_array)
-        ];
-        $conflicts[] = [
-            "Value Conflicts" => $this->compareValues($assignments_array)
-        ];
+        $conflicts = array_merge(
+            $this->compareStructures($assignments_array),
+            $this->compareScopes($assignments_array),
+            $this->compareValues($assignments_array)
+        );
 
         return $conflicts;
     }
 
+    /*-------------------------------*/
+    /* Conflict Detection Algorithms */
+    /*-------------------------------*/
     private function compareStructures($assignments_array){
-        $structure_conflicts = [];                                  // A place to store conflicts
+        $structure_conflicts = [];                                              // A place to store conflicts
         /* Start Scan */
-        $inputs = [];                                               // A place to store the different user responses
-        foreach($assignments_array as $assignment) {                // For every assignment we are comparing
-            $input = $this->getBranchCount($assignment);    // Get it's number of branches
+        $inputs = [];                                                           // A place to store the different user responses
+        foreach($assignments_array as $assignment) {                            // For every assignment we are comparing
+            $input = $this->getBranchCount($assignment);                        // Get it's number of branches
             $inputs[$input][] = $assignment["_key"];
         }
-        if(count($inputs) > 1) $structure_conflicts[] = $inputs;    // More than one response was recorded
+        if(count($inputs) > 1){                                                 // More than one response was recorded
+            $structure_conflicts[] = $this->newConflict("structure", [
+                "inputs" => $inputs
+            ]);
+        }
         /* End Scan */
         return $structure_conflicts;
     }
-
     private function compareScopes($assignments_array){
         $scope_conflicts = [];
         /* Start Scan */
@@ -39,16 +39,16 @@ class ConflictManager {
                 $input = $this->getScope($fieldName, $assignment);   // Check what they said the field's scope was
                 $inputs[$input][] = $assignment["_key"];               // Record their response into inputs
             }
-//            if(count($inputs) > 1) ...                             // If there were multiple responses
-            $scope_conflicts[] = [
-                "Variable" => $fieldName,
-                "Scope Inputs" => $inputs
-            ];
+            if(count($inputs) > 1){
+                $scope_conflicts[] = $this->newConflict("scope", [
+                    "variable" => $fieldName,
+                    "inputs" => $inputs
+                ]);
+            }                             // If there were multiple responses
         }
         /* End Scan */
         return $scope_conflicts;
     }
-
     private function compareValues($assignments_array){
         $value_conflicts = [];
         /* Start Scan */
@@ -59,22 +59,21 @@ class ConflictManager {
                 $input = $this->getInput($fieldName, $assignment);   // Check what they said the field's scope was
                 $inputs[$input][] = $assignment["_key"];               // Record their response into inputs
             }
-//            if(count($inputs) > 1) ...                             // If there were multiple responses
-            $value_conflicts[] = [
-                "Variable" => $fieldName,
-                "Value Inputs" => $inputs
-            ];
+            if(count($inputs) > 1) {                                    // If there were multiple responses
+                $value_conflicts[] = $this->newConflict("value", [
+                    "variable" => $fieldName,
+                    "inputs" => $inputs
+                ]);
+            }
+
         }
         /* End Scan */
         return $value_conflicts;
     }
 
-
     /*------------------*/
     /* Helper Functions */
     /*------------------*/
-
-    // Caleb implement these
     private function getScope($fieldName, $assignment){
         if(mt_rand(0,1) === 0){
             return "variable";
@@ -95,12 +94,18 @@ class ConflictManager {
         return count($assignment['encoding']['branches']);
     }
 
+    private function newConflict($type, $body){
+        return [
+            "type" => $type,
+            "body" => $body
+        ];
+    }
+
     /*----------------*/
     /* Initialization */
     /*----------------*/
     public $variables;
-    public function __construct()
-    {
+    public function __construct() {
         $this->variables = json_decode("[ \"acclimationDuration\", \"acclimationPeriod\", \"ageAtStart\", \"ageAtWeight\", \"airCirculation\", \"animalLocations\", \"averageFinalWeight\", \"beddingMaterial\", \"breed\", \"cageType\", \"changeFrequency\", \"compoundFrequency\", \"compoundName\", \"constantTemperature\", \"darkHours\", \"daysOnTreatment\", \"dietID\", \"dietType\", \"dietVendor\", \"dietVendorCity\", \"dosage\", \"enrichmentType\", \"errorOfMeasurmentType\", \"errorOfMeasurmentValue\", \"ethicalStatement\", \"exerciseFreq\", \"exerciseType\", \"facilityCityState\", \"facilityCountry\", \"facilityHumidity\", \"facilityName\", \"feedingFrequency\", \"forcedExcecise\", \"geneticManipulationType\", \"lightHours\", \"lightingSchedule\", \"lightStartTime\", \"micePerCage\", \"mouseVendorName\", \"pathogenFreeEnvironment\", \"percentCarbohydrates\", \"percentEnergy\", \"percentFat\", \"percentProtein\", \"routeOfAdministration\", \"sampleSize\", \"sex\", \"surgeryType\", \"temperatureRange\", \"vendorCountry\", \"vendorName\", \"whereReported\" ]", true);
     }
 }
