5b010d724a0f30ca114d5a8031f343afc2fbbe4c
Chris Rocco <chris.rocco7@gmail.com>
2017-04-22 22:56:09 -0500

    Refractored assignment routes to use data handlers

--- src/routes/study_routes.php
+++ src/routes/study_routes.php
@@ -1,22 +1,4 @@
 <?php
-// set up some aliases for less typing later
-use ArangoDBClient\Collection as ArangoCollection;
-use ArangoDBClient\CollectionHandler as ArangoCollectionHandler;
-use ArangoDBClient\Connection as ArangoConnection;
-use ArangoDBClient\ConnectionOptions as ArangoConnectionOptions;
-use ArangoDBClient\DocumentHandler as ArangoDocumentHandler;
-use ArangoDBClient\EdgeHandler as ArangoEdgeHandler;
-use ArangoDBClient\Document as ArangoDocument;
-use ArangoDBClient\Edge as ArangoEdge;
-use ArangoDBClient\Exception as ArangoException;
-use ArangoDBClient\Export as ArangoExport;
-use ArangoDBClient\ConnectException as ArangoConnectException;
-use ArangoDBClient\ClientException as ArangoClientException;
-use ArangoDBClient\ServerException as ArangoServerException;
-use ArangoDBClient\Statement as ArangoStatement;
-use ArangoDBClient\UpdatePolicy as ArangoUpdatePolicy;
-
-// Routes
 
 /*
  * GET studies/{studyname}/structure
