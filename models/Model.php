0aaa70ee94acbe9fdcde8171bc307baff5478a5e
= <=>
2017-04-29 12:42:58 -0500

    Test case is written and failing. Ready to start

---
+++ models/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 4/29/17
+ * Time: 12:41 PM
+ */
+
+namespace Models;
+
+
+class Model {
+
+}
--- tests/users/LoginTest.php
+++ tests/users/LoginTest.php
@@ -2,12 +2,25 @@
 
 namespace Tests\Users;
 
+use Entities\User;
 use Tests\BaseTestCase;
 
 class LoginTest extends BaseTestCase {
 
+    static $testUser = [
+        'email'     =>  'chris.rocco7@gmail.com',
+        'password'  =>  'password'
+    ];
+
     public function testLogin(){
-        self::assertTrue(true);
+
+        $result = User::login(
+            self::$testUser['email'],
+            self::$testUser['password']
+        );
+
+        self::assertFalse($result == User::INVALID);
+        self::assertFalse( !$result );
     }
 
 }
