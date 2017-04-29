3e3df77b207194f94de8752bf4bbec3040dc888e
= <=>
2017-04-29 18:09:03 -0500

    Ex Settings

--- src/ex_settings.php
+++ src/ex_settings.php
@@ -1,5 +1,34 @@
 <?php
 $settings = [
+    // JWT secret
+    'JWT_secret' => 'supersecretkeyyoushouldnotcommitotgithub',
+
+    // Database Connection
+    "arangodb_connection_options" => [
+        // database name
+        \triagens\ArangoDb\ConnectionOptions::OPTION_DATABASE => 'bigdata',
+        // server endpoint to connect to
+        \triagens\ArangoDb\ConnectionOptions::OPTION_ENDPOINT => 'tcp://______:8529',
+        // authorization type to use (currently supported: 'Basic')
+        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
+        // user for basic authorization
+        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_USER => 'username',
+        // password for basic authorization
+        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_PASSWD => 'password',
+        // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
+        \triagens\ArangoDb\ConnectionOptions::OPTION_CONNECTION => 'Keep-Alive',
+        // connect timeout in seconds
+        \triagens\ArangoDb\ConnectionOptions::OPTION_TIMEOUT => 3,
+        // whether or not to reconnect when a keep-alive connection has timed out on server
+        \triagens\ArangoDb\ConnectionOptions::OPTION_RECONNECT => true,
+        // optionally create new collections when inserting documents
+        \triagens\ArangoDb\ConnectionOptions::OPTION_CREATE => true,
+        // optionally create new collections when inserting documents
+        \triagens\ArangoDb\ConnectionOptions::OPTION_UPDATE_POLICY => \triagens\ArangoDb\UpdatePolicy::LAST,
+    ],
+    "arangodb_development_connection_options"   =>  [ /* same as above */ ],
+
+    // Slim Project Settings
     'displayErrorDetails' => true, // set to false in production
     'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
@@ -10,52 +39,14 @@ $settings = [
         'level' => \Monolog\Logger::DEBUG,
     ],
 
-    // JWT secret
-    'JWT_secret' => 'supersecretkeyyoushouldnotcommittogithub',
-
-    "arangodb_connection_options" => [
-        // database name
-        \triagens\ArangoDb\ConnectionOptions::OPTION_DATABASE => 'database_name',
-        // server endpoint to connect to
-        \triagens\ArangoDb\ConnectionOptions::OPTION_ENDPOINT => 'tcp://____________:8529',
-        // authorization type to use (currently supported: 'Basic')
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
-        // user for basic authorization
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_USER => 'database_username',
-        // password for basic authorization
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_PASSWD => 'database_password',
-        // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
-        \triagens\ArangoDb\ConnectionOptions::OPTION_CONNECTION => 'Keep-Alive',
-        // connect timeout in seconds
-        \triagens\ArangoDb\ConnectionOptions::OPTION_TIMEOUT => 3,
-        // whether or not to reconnect when a keep-alive connection has timed out on server
-        \triagens\ArangoDb\ConnectionOptions::OPTION_RECONNECT => true,
-        // optionally create new collections when inserting documents
-        \triagens\ArangoDb\ConnectionOptions::OPTION_CREATE => true,
-        // optionally create new collections when inserting documents
-        \triagens\ArangoDb\ConnectionOptions::OPTION_UPDATE_POLICY => \triagens\ArangoDb\UpdatePolicy::LAST,
-    ],
-
-    "arangodb_development_connection_options"   =>  [
-        \triagens\ArangoDb\ConnectionOptions::OPTION_DATABASE => 'database_name',
-        // server endpoint to connect to
-        \triagens\ArangoDb\ConnectionOptions::OPTION_ENDPOINT => 'tcp://____________:8529',
-        // authorization type to use (currently supported: 'Basic')
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
-        // user for basic authorization
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_USER => 'database_username',
-        // password for basic authorization
-        \triagens\ArangoDb\ConnectionOptions::OPTION_AUTH_PASSWD => 'database_password',
-        // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
-        \triagens\ArangoDb\ConnectionOptions::OPTION_CONNECTION => 'Keep-Alive',
-        // connect timeout in seconds
-        \triagens\ArangoDb\ConnectionOptions::OPTION_TIMEOUT => 3,
-        // whether or not to reconnect when a keep-alive connection has timed out on server
-        \triagens\ArangoDb\ConnectionOptions::OPTION_RECONNECT => true,
-        // optionally create new collections when inserting documents
-        \triagens\ArangoDb\ConnectionOptions::OPTION_CREATE => true,
-        // optionally create new collections when inserting documents
-        \triagens\ArangoDb\ConnectionOptions::OPTION_UPDATE_POLICY => \triagens\ArangoDb\UpdatePolicy::LAST,
+    // Mail Server
+    'smtp'  =>  [
+        'host'          =>  'smtp.gmail.com',
+        'smtp_auth'     =>  true,
+        'username'      =>  '________@gmail.com',
+        'password'      =>  '_______________',
+        'smtp_secure'   =>  'tls',
+        'port'          =>  587
     ]
 ];
 
