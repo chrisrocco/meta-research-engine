ebee737dae2576915100c602ff12c1fbfced0f19
= <=>
2017-04-29 12:37:17 -0500

    Test Cases

---
+++ tests/users/LoginTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Users;
+
+use Tests\BaseTestCase;
+
+class LoginTest extends BaseTestCase {
+
+    public function testLogin(){
+        self::assertTrue(true);
+    }
+
+}
