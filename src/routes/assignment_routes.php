051b4d93fd7088bb96f596f0d3acdbc1d5cb7d19
= <=>
2017-05-01 01:45:00 -0500

    POST /assignments

--- src/routes/assignment_routes.php
+++ src/routes/assignment_routes.php
@@ -1,56 +1,61 @@
 <?php
-use Entities\Assignment as Assignment;
+use \Models\Vertices\User;
+use Models\Vertices\Paper;
+use Models\Edges\Assignment;
 
-/**
- * GET assignmentsIDGet
- * Summary: Returns a single assignment
- * Notes:
- * Output-Formats: [application/json]
- */
-$app->GET('/assignments/{ID}', function ($request, $response, $args) {
-    $assignment = new Assignment($args['ID']);
-    $assignmentObject = $assignment->getAssignment();
-    return $response->write(json_encode($assignmentObject,  JSON_PRETTY_PRINT));
-});
-
-/**
- * PUT assignmentsIDPut
- * Summary: Updates assignment with a students work
- * Notes:
- */
-$app->PUT('/assignments/{ID}', function ($request, $response, $args) {
-    $formData = $request->getParams();
-
-    $assignment = new Assignment($args['ID']);
-    $assignment->update($formData);
-
-    return $response
-        ->write("Updated Assignment " . $args['ID'])
-        ->withStatus(200);
-});
-
-/**
- * GET studentsIDAssignmentsGet
- * Summary: Returns a list of assignments to a student
- * Notes:
- * Output-Formats: [application/json]
- */
-$app->GET('/users/{ID}/assignments', function ($request, $response, $args) {
-    $user = new UserHandler($args["ID"]);
-    $assignments = $user->getAssignments();
-    return $response->write(json_encode($assignments, JSON_PRETTY_PRINT));
-});
+///**
+// * GET assignmentsIDGet
+// * Summary: Returns a single assignment
+// * Notes:
+// * Output-Formats: [application/json]
+// */
+//$app->GET('/assignments/{ID}', function ($request, $response, $args) {
+//    $assignment = new Assignment($args['ID']);
+//    $assignmentObject = $assignment->getAssignment();
+//    return $response->write(json_encode($assignmentObject,  JSON_PRETTY_PRINT));
+//});
+//
+///**
+// * PUT assignmentsIDPut
+// * Summary: Updates assignment with a students work
+// * Notes:
+// */
+//$app->PUT('/assignments/{ID}', function ($request, $response, $args) {
+//    $formData = $request->getParams();
+//
+//    $assignment = new Assignment($args['ID']);
+//    $assignment->update($formData);
+//
+//    return $response
+//        ->write("Updated Assignment " . $args['ID'])
+//        ->withStatus(200);
+//});
+//
+///**
+// * GET studentsIDAssignmentsGet
+// * Summary: Returns a list of assignments to a student
+// * Notes:
+// * Output-Formats: [application/json]
+// */
+//$app->GET('/users/{ID}/assignments', function ($request, $response, $args) {
+//    $user = new UserHandler($args["ID"]);
+//    $assignments = $user->getAssignments();
+//    return $response->write(json_encode($assignments, JSON_PRETTY_PRINT));
+//});
 
 /**
  * POST studentsIDAssignmentsPost
  * Summary: Creates an assignment to a student
  * Notes:
  */
-$app->POST('/users/{ID}/assignments', function ($request, $response, $args) {
-    $pmcID = $request->getParam("pmcID");
+$app->POST('/assignments', function ($request, $response, $args) {
+    $paperKey = $request->getParam("paperKey");
+    $userKey = $request->getParam("userKey");
 
-    $user = new UserHandler($args['ID']);
-    $user->createAssignment($pmcID);
 
+    $user = User::retrieve( $userKey );
+    $paper = Paper::retrieve( $paperKey );
+
+    Assignment::assign( $paper, $user );
     $response->write("Created Assignment");
 });
