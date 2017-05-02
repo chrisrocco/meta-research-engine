c1d74a5f5627673102c39ac45530d101990edf74
= <=>
2017-05-02 00:22:28 -0500

    gibberish

---
+++ lib/papers/Response.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Papers;
+
+/**
+ * User: chris
+ * Date: 5/1/17
+ * Time: 11:41 AM
+ */
+class Response
+{
+    public $content;
+    public $user_keys = [];
+
+    /**
+     * @param $other Response
+     */
+    function compare( $other ){
+
+    }
+}
