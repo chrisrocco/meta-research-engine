e0feeeaacfb373c156a18596fd53bff42f53d1cd
Chris Rocco <chris.rocco7@gmail.com>
2017-05-04 22:50:02 -0500

    Bug Fixes

--- data/scripts/examples/bulk-assignment.php
+++ data/scripts/examples/bulk-assignment.php
@@ -4,19 +4,23 @@
  * User: chris
  * Date: 4/30/17
  * Time: 1:47 PM
+ *
+ * Generates some random users and papers
+ *
+ * Each paper is double encoded to two random students
  */
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
 use \triagens\ArangoDb;
-
-
-\DB\DB::enterDevelopmentMode();
+use \Models\Vertices\User;
+use \Models\Vertices\Paper;
+use \Models\Edges\Assignment;
 
 // Make some users
 $users = [];
 for ($i = 0; $i < 20; $i++){
-    $users[] = \Models\User::create([
+    $users[] = User::create([
         'first_name'    =>  'User ' . $i,
         'last_name'     =>  ' ',
         'email'         =>  $i . '@gmail.com',
@@ -29,7 +33,7 @@ for ($i = 0; $i < 20; $i++){
 // Make some papers
 $papers = [];
 for ($i = 0; $i < 20; $i++){
-    $papers[] = \Models\Paper::create([
+    $papers[] = Paper::create([
         'Title' =>  'Study #' . $i,
         'pmcID' =>  (rand(1000000, 9000000))
     ]);
@@ -42,8 +46,8 @@ foreach ( $papers as $paper ){
     $randomUser = $users[ rand(0, count($users)-1) ];
     $randomUser2 = $users[ rand(0, count($users)-1) ];
 
-    $a1 = \Models\Assignment::assign( $paper, $randomUser )->id();
-    $a2 = \Models\Assignment::assign( $paper, $randomUser2 )->id();
+    $a1 = Assignment::assign( $paper, $randomUser )->id();
+    $a2 = Assignment::assign( $paper, $randomUser2 )->id();
 
     print "Created assignment \n";
 }
@@ -53,9 +57,9 @@ foreach ( $papers as $paper ){
 $double_encoded_graph = new ArangoDb\Graph('assignments_PHP');
 
 $assignment = new ArangoDb\EdgeDefinition();
-$assignment->setRelation( \Models\Assignment::$collection );
-$assignment->addToCollection( \Models\User::$collection );
-$assignment->addFromCollection( \Models\Paper::$collection );
+$assignment->setRelation( Assignment::$collection );
+$assignment->addToCollection( User::$collection );
+$assignment->addFromCollection( Paper::$collection );
 
 $double_encoded_graph->addEdgeDefinition($assignment);
 
