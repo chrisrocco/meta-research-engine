fae2f1fab4b1015509becc88f6907658ab573d54
Chris Rocco <chris.rocco7@gmail.com>
2017-05-15 01:44:56 -0500

    still trying

--- public/index.php
+++ public/index.php
@@ -18,6 +18,16 @@ $connection = new \triagens\ArangoDb\Connection($config);
 
 // Instantiate the Slim App
 $app = new \Slim\App($settings);
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
