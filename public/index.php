05ca0da059a7a5cffd72aef58af2f0e1cf9785c5
Chris Rocco <chris.rocco7@gmail.com>
2017-05-15 01:38:23 -0500

    Enabled CORS

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $settings = require __DIR__ . '/../src/settings.php';
