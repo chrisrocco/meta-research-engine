3f2d323287ba2edc34d8d22e8dbff81ab7794670
Chris Rocco <chris.rocco7@gmail.com>
2017-06-06 08:26:46 -0500

    --

--- src/middleware.php
+++ src/middleware.php
@@ -1,10 +1,10 @@
 <?php
 // Application middleware
 
-$app->add(new \Slim\Middleware\JwtAuthentication([
+$app->add( new \Slim\Middleware\JwtAuthentication([
 	"path" => ["/"],
     "passthrough" => ["/users/", "/reportError"],
 
     "secure" => false,
     "secret" => $app->getContainer()->get("settings")['JWT_secret'],
-]));
+]) );
