b8f803e09f44c12d6d22eb38cb80bce4d33564bd
Chris Rocco <chris.rocco7@gmail.com>
2017-06-06 17:19:42 -0500

    server-side crash reporting

--- public/index.php
+++ public/index.php
@@ -59,3 +59,79 @@ require( __DIR__ . "/../src/routes/application_routes.php");
 
 // Run App
 $app->run();
+
+
+
+// Handle Errors
+
+register_shutdown_function( "fatal_handler" );
+
+function fatal_handler() {
+    $errfile = "unknown file";
+    $errstr  = "shutdown";
+    $errno   = E_CORE_ERROR;
+    $errline = 0;
+
+    $error = error_get_last();
+
+    if( $error !== NULL) {
+        $errno   = $error["type"];
+        $errfile = $error["file"];
+        $errline = $error["line"];
+        $errstr  = $error["message"];
+
+        global $settings;
+
+        $mail = new \PHPMailer();
+        $mail->isSMTP();
+        $mail->Host         =   $settings['smtp']['host'];
+        $mail->SMTPAuth     =   $settings['smtp']['smtp_auth'];
+        $mail->Username     =   $settings['smtp']['username'];
+        $mail->Password     =   $settings['smtp']['password'];
+        $mail->SMTPSecure   =   $settings['smtp']['smtp_secure'];
+        $mail->Port         =   $settings['smtp']['port'];
+
+        $mail->From         =   "error_reporting@researchcoder.com";
+        $mail->FromName     =   "Crash Reporter";
+
+        $mail->addAddress("chris.rocco7@gmail.com", "Chris Rocco");
+        $mail->addAddress("caleb.falcione@gmail.com", "Caleb Falcione");
+        $mail->isHTML(true);
+
+        $mail->Subject = "Researchcoder.com Crash Report";
+        $mail->Body = format_error( $errno, $errstr, $errfile, $errline);
+    }
+}
+
+function format_error( $errno, $errstr, $errfile, $errline ) {
+    $trace = print_r( debug_backtrace( false ), true );
+
+    $content = "
+  <table>
+  <thead><th>Item</th><th>Description</th></thead>
+  <tbody>
+  <tr>
+    <th>Error</th>
+    <td><pre>$errstr</pre></td>
+  </tr>
+  <tr>
+    <th>Errno</th>
+    <td><pre>$errno</pre></td>
+  </tr>
+  <tr>
+    <th>File</th>
+    <td>$errfile</td>
+  </tr>
+  <tr>
+    <th>Line</th>
+    <td>$errline</td>
+  </tr>
+  <tr>
+    <th>Trace</th>
+    <td><pre>$trace</pre></td>
+  </tr>
+  </tbody>
+  </table>";
+
+    return $content;
+}
