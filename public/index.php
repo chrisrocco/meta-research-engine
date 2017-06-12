ad075134cdf741020d9fbd33a5c7b2123642842a
Chris Rocco <chris.rocco7@gmail.com>
2017-06-12 18:25:13 -0500

    error handling

--- public/index.php
+++ public/index.php
@@ -62,78 +62,30 @@ try {
     $app->run();
 } catch ( Exception $e ) {
     fatal_handler();
-    var_dump( $e );
-}
+    $mail = new \PHPMailer();
+    $mail->isSMTP();
+    $mail->Host         =   $settings['smtp']['host'];
+    $mail->SMTPAuth     =   $settings['smtp']['smtp_auth'];
+    $mail->Username     =   $settings['smtp']['username'];
+    $mail->Password     =   $settings['smtp']['password'];
+    $mail->SMTPSecure   =   $settings['smtp']['smtp_secure'];
+    $mail->Port         =   $settings['smtp']['port'];
 
-function fatal_handler() {
-    $errfile = "unknown file";
-    $errstr  = "shutdown";
-    $errno   = E_CORE_ERROR;
-    $errline = 0;
+    $mail->From         =   "error_reporting@researchcoder.com";
+    $mail->FromName     =   "Crash Reporter";
 
-    $error = error_get_last();
+    $mail->addAddress("chris.rocco7@gmail.com", "Chris Rocco");
+    $mail->addAddress("caleb.falcione@gmail.com", "Caleb Falcione");
+    $mail->isHTML(true);
 
-    if( $error !== NULL) {
+    $mail->Subject = "Researchcoder.com Crash Report";
 
-        $errno   = $error["type"];
-        $errfile = $error["file"];
-        $errline = $error["line"];
-        $errstr  = $error["message"];
+    $message = "File : " . $e->getFile() . PHP_EOL;
+    $message = "Message : " . $e->getMessage() . PHP_EOL;
+    $message = "Line : " . $e->getLine() . PHP_EOL;
+    $message = "Code : " . $e->getCode() . PHP_EOL;
+    $message = "Trace : " . $e->getTraceAsString() . PHP_EOL;
 
-        global $settings;
-
-        $mail = new \PHPMailer();
-        $mail->isSMTP();
-        $mail->Host         =   $settings['smtp']['host'];
-        $mail->SMTPAuth     =   $settings['smtp']['smtp_auth'];
-        $mail->Username     =   $settings['smtp']['username'];
-        $mail->Password     =   $settings['smtp']['password'];
-        $mail->SMTPSecure   =   $settings['smtp']['smtp_secure'];
-        $mail->Port         =   $settings['smtp']['port'];
-
-        $mail->From         =   "error_reporting@researchcoder.com";
-        $mail->FromName     =   "Crash Reporter";
-
-        $mail->addAddress("chris.rocco7@gmail.com", "Chris Rocco");
-        $mail->addAddress("caleb.falcione@gmail.com", "Caleb Falcione");
-        $mail->isHTML(true);
-
-        $mail->Subject = "Researchcoder.com Crash Report";
-        $mail->Body = format_error( $errno, $errstr, $errfile, $errline);
-
-        $mail->send();
-    }
-}
-
-function format_error( $errno, $errstr, $errfile, $errline ) {
-    $trace = print_r( debug_backtrace( false ), true );
-
-    $content = "
-  <table>
-  <thead><th>Item</th><th>Description</th></thead>
-  <tbody>
-  <tr>
-    <th>Error</th>
-    <td><pre>$errstr</pre></td>
-  </tr>
-  <tr>
-    <th>Errno</th>
-    <td><pre>$errno</pre></td>
-  </tr>
-  <tr>
-    <th>File</th>
-    <td>$errfile</td>
-  </tr>
-  <tr>
-    <th>Line</th>
-    <td>$errline</td>
-  </tr>
-  <tr>
-    <th>Trace</th>
-    <td><pre>$trace</pre></td>
-  </tr>
-  </tbody>
-  </table>";
-
-    return $content;
+    $mail->Body = $message;
+    $mail->send();
 }
